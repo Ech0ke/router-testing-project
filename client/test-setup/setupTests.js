1f8cf995c956ab5483cdc600eb626e4f0f133825
Ech0ke <aidas.zygas@gmail.com>
2023-09-18 12:16:07 +0300

    Added server mocked server to use as an api

--- client/test-setup/setupTests.js
+++ client/test-setup/setupTests.js
@@ -1,9 +1,18 @@
 import * as matchers from "@testing-library/jest-dom/matchers";
-import { afterEach, expect } from "vitest";
+import { afterAll, afterEach, beforeAll, expect } from "vitest";
 import { cleanup } from "@testing-library/react";
+import { mockServer } from "./mockServer";
 
 expect.extend(matchers);
 
+beforeAll(() => {
+  mockServer.listen({ onUnhandledRequest: "error" });
+});
+
 afterEach(() => {
   cleanup();
+});
+
+afterAll(() => {
+  mockServer.close();
 });
